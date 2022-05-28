32a8aaaf7410d7a2fbd17174e62a76567959d572
tranv2 <tranhoangv698@gmail.com>
2022-05-29 00:22:26 +0700

    create buttons movetoPage and movetoReport but not yet created the pages for each of them

--- src/main/java/com/example/hotelmanagement/HelloApplication.java
+++ src/main/java/com/example/hotelmanagement/HelloApplication.java
@@ -7,15 +7,16 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("home.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        fxmlLoader.getController();
         stage.setTitle("Hotel Management");
         stage.setScene(scene);
-        stage.setFullScreen(true);
         stage.show();
 
     }
--- src/main/java/com/example/hotelmanagement/HomeController.java
+++ src/main/java/com/example/hotelmanagement/HomeController.java
@@ -1,22 +1,108 @@
 package com.example.hotelmanagement;
 
-import javafx.beans.property.ReadOnlyDoubleProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.AbstractMap;
+import java.util.HashMap;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 public class HomeController implements Initializable {
 
     @FXML
+    private Button btnCheckIn;
+
+    @FXML
+    private Button btnCheckOut;
+
+    @FXML
+    private Button btnHallReservation;
+
+    @FXML
+    private Button btnRoomReservation;
+
+    @FXML
+    private Button btnRoomService;
+
+    @FXML
+    private Button btnVoucher;
+
+    @FXML
     private VBox leftnavigation;
-    public HomeController() {}
+
+    @FXML
+    private Button reportAllTransactions;
+
+    @FXML
+    private Button reportCheckOutDue;
+
+    @FXML
+    private Button reportDayBook;
+
+    @FXML
+    private Button reportDebtors;
+
+    @FXML
+    private Button reportHallReservation;
+
+    @FXML
+    private Button reportOverall;
+
+    @FXML
+    private Button reportRoomOccupancy;
+
+    @FXML
+    private Button reportRoomReservation;
+    HashMap<Integer, String> reports = new HashMap<Integer, String>();
+    HashMap<Integer, String> actions = new HashMap<Integer, String>();
+
+    public void moveToReport(ActionEvent event) throws IOException {
+        Button clicked = (Button) event.getSource();
+        String reportClickedId = clicked.getId();
+        Set<Integer> keySet = reports.keySet();
+        for (Integer key: keySet) {
+            if (reports.get(key) == reportClickedId) {
+                // To that Window
+            }
+        }
+    }
+
+    public void moveToPage(ActionEvent event) throws IOException {
+        Button clicked = (Button) event.getSource();
+        String buttonClickedId = clicked.getId();
+        Set<Integer> keySet = actions.keySet();
+        for (Integer key : keySet) {
+            if (actions.get(key) == buttonClickedId) {
+                // Display action choices in the same window
+            }
+        }
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        leftnavigation.setPrefSize(300.0, 720.0);
-        leftnavigation.setAlignment(Pos.CENTER);
+        // Initialize for Report Buttons
+        reports.put(1, "reportDayBook");
+        reports.put(2, "reportCheckOutDue");
+        reports.put(3, "reportOverall");
+        reports.put(4, "reportRoomOccupancy");
+        reports.put(5, "reportHallReservation");
+        reports.put(6, "reportRoomReservation");
+        reports.put(7, "reportAllTransactions");
+        reports.put(8, "reportDebtors");
+
+        // Initialize for Action Buttons
+        actions.put(1, "btnCheckIn");
+        actions.put(2, "btnCheckOut");
+        actions.put(3, "btnRoomReservation");
+        actions.put(4, "btnHallReservation");
+        actions.put(5, "btnRoomService");
+        actions.put(6, "btnVoucher");
     }
 }
+
