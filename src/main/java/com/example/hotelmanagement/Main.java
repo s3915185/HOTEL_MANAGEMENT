7fe6573eb92de1264630562d8b753f8305e0ed59
tranv2 <tranhoangv698@gmail.com>
2022-05-29 13:33:58 +0700

    add in logos

--- src/main/java/com/example/hotelmanagement/Main.java
+++ src/main/java/com/example/hotelmanagement/Main.java
@@ -3,6 +3,7 @@ package com.example.hotelmanagement;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +14,7 @@ public class Main extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("home.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         fxmlLoader.getController();
+        stage.getIcons().add(new Image("file:src/main/resources/images/logo/VIVANT PURPLE BACKGROUND.png"));
         stage.setTitle("Hotel Management");
         stage.setScene(scene);
         stage.show();
