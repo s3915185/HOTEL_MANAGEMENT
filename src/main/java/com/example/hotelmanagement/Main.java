49117a63c4e5399858d2fe3bcf28753c6fe7443c
tranv2 <tranhoangv698@gmail.com>
2022-05-29 14:43:14 +0700

    display simple fxml for actions

--- src/main/java/com/example/hotelmanagement/Main.java
+++ src/main/java/com/example/hotelmanagement/Main.java
@@ -14,8 +14,8 @@ public class Main extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("home.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         fxmlLoader.getController();
-        stage.getIcons().add(new Image("file:src/main/resources/images/logo/VIVANT PURPLE BACKGROUND.png"));
-        stage.setTitle("Hotel Management");
+        stage.getIcons().add(new Image("file:src/main/resources/images/logo/VIVANT_PURPLE_BACKGROUND.png"));
+        stage.setTitle("VIVANT®");
         stage.setScene(scene);
         stage.show();
 
